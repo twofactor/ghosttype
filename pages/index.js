e26ef883a5c936fa2cdd2d95a4dc5c204a44f225
David Bui <david@Davids-MBP.home>
2020-05-12 23:52:50 -0700

    edit home page

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import {
   Box,
   Link,
   Divider,
+  Avatar,
 } from "@chakra-ui/core";
 
 import { Container } from "../components/container";
@@ -53,32 +54,18 @@ export default function Home() {
           <Text fontSize="lg" mb="12px">
             Create a minimal, barebones style blog in seconds. You know, like
             those jekyll with gatsby on netlify or github pages blogs you've
-            been too lazy so set up.
+            been too lazy so set up yourself.
           </Text>
-
           <Link href="/api/login">
             <Button width="100%" variant="outline" size="md" mb="12px">
               <Text fontSize="md">Sign Up</Text>
             </Button>
           </Link>
           <Divider mb="28px" />
-
-          <Text fontSize="lg" mb="12px">
-            I mostly made this because I was too lazy to set that stuff up
-            myself and all the existing blogging platforms felt so bloated and
-            overdone, so I figured maybe other people would feel the same as
-            well.
-          </Text>
-          <Text fontSize="lg" mb="12px">
-            There's something about blogs I really like and miss from the old
-            internet. All the content these days feels so overly commercialized
-            or bloated, and the only closest proxy to a classic blog is
-            newsletters, which feel too ephemeral and not timeless the way blog
-            posts are.
-          </Text>
           <Text fontSize="lg" mb="24px">
-            You can check out some example blogs if you'd like as well. If you
-            want me to feature yours, just dm me on twitter
+            You can check out some example blogs if you'd like as well! If you
+            have one, think its cool, and want it to be featured on the
+            homepage, just hop on twitter and shoot me a DM
             <Link
               target="_blank"
               ml="4px"
@@ -102,23 +89,71 @@ export default function Home() {
           </Box>
           <Box height="32px" />
           <Text fontSize="lg" fontWeight="bold">
+            Why does this exist?
+          </Text>
+          <Text fontSize="lg" mb="12px">
+            I built this because I wanted to set up a hyperminimalist blog. All
+            the existing blogging/content platforms felt overly bloated and
+            commercial, and newsletters felt too ephemerial - I wanted content
+            to stand the test of time to be looked back at later, not to be
+            forgotten within a week.
+          </Text>{" "}
+          <Text fontSize="lg" mb="12px">
+            The closest parallel I could find to what I was looking for were
+            these Jekyll blogs quite a few people use. Unfortuantely I was too
+            lazy to figure out how to use ruby to get jekyll working, So I
+            figured maybe other people might be in the same boat as well.
+          </Text>
+          <Text fontSize="lg" fontWeight="bold">
             Are there more features coming soon?
           </Text>
           <Text fontSize="lg" mb="12px">
             Yeah I'll add little things here and there as people ask for things
             and I figure out what's missing. Feel free to contact me if you have
-            a feature request!
+            a feature request! Or an idea. Or found a bug. We don't like bugs.
+          </Text>
+          <Text fontSize="lg" mb="12px">
+            Maybe I might slide some social features here and there so you can
+            follow/subscribe to your favorite blogs and friends as well, but I
+            want to keep the spirt of this platform intact so who knows.
           </Text>
           <Text fontSize="lg" fontWeight="bold">
             Is this going to stay up forever?
           </Text>
           <Text fontSize="lg" mb="12px">
             I hope so? Ideally I'd like to keep it up and free forever but
-            databases aren't free so if people use this enough to the point
-            where I can't afford to keep it up I'll have to charge or something.
-            If you'd like to donate database/cloud credits to me let me know as
-            well!
+            servers and databases aren't free so if people use this enough to
+            the point where I can't afford to keep it up I'll have to charge a
+            small fee/donation or something. If you'd like to donate
+            database/cloud credits to me let me know as well!
           </Text>
+          <Text fontSize="lg" fontWeight="bold">
+            Any other useful things to know?
+          </Text>
+          <Text fontSize="lg" mb="12px">
+            Yeah! If you're logged in, you can quickly go to your admin
+            dashboard by clicking on your profile picture and name in the top
+            right corner.
+          </Text>
+          <Text fontSize="lg" mb="12px">
+            Markdown shortcuts also work when you're writing a post, so you can
+            type # or ## or * for headings/lists/etc.
+          </Text>
+          <Text fontSize="lg" mb="12px">
+            Also, I'm looking for more twitter followers. Feel free to
+          </Text>
+          <a target="_blank" href="http://twitter.com/ghosttyped">
+            <Button variant="outline" pl="8px" pr="12px">
+              <Avatar
+                size="xs"
+                name="ghosttyped"
+                src="https://pbs.twimg.com/profile_images/1254546118731390978/vC_IGyQe_normal.jpg"
+              />
+              <Text fontSize="lg" ml="8px">
+                Follow David On Twitter
+              </Text>
+            </Button>
+          </a>
           <Box height="200px" />
         </Column>
       </Container>

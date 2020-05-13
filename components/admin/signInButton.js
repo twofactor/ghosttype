208ac92836dffdf1eed7f026f25c8ac25725e237
David Bui <david@Davids-MBP.home>
2020-05-13 00:18:34 -0700

    lets not break the api

--- components/admin/signInButton.js
+++ components/admin/signInButton.js
@@ -5,32 +5,31 @@ import { Button, Text, Avatar, Skeleton } from "@chakra-ui/core";
 import fetch from "../../lib/fetch";
 
 export default function SignInButton() {
-  const { data, error } = useSWR("/api/me", fetch);
+  // const { data, error } = useSWR("/api/me", fetch);
 
-  if (!data) {
-    return (
-      <Skeleton>
-        <Button variant="outline" size="sm">
-          Sign In/Sign Up
-        </Button>
-      </Skeleton>
-    );
-  } else if (!data.nickname) {
-    return (
-      <a href="/api/login">
-        <Button variant="outline" size="sm">
-          <Text fontSize="sm">Sign In/Sign Up</Text>
-        </Button>
-      </a>
-    );
-  } else {
-    return (
-      <NextLink href="/admin">
-        <Button pl="6px" pr="6px" variant="outline" size="sm">
-          <Avatar size="xs" mr="8px" name={data.name} src={data.picture} />
-          <Text fontSize="sm">{data.nickname}</Text>
-        </Button>
-      </NextLink>
-    );
-  }
+  // if (!data) {
+  //   return (
+  //     <Skeleton>
+  //       <Button variant="outline" size="sm">
+  //         Sign In/Sign Up
+  //       </Button>
+  //     </Skeleton>
+  //   );
+  // } else if (!data.nickname) {
+  return (
+    <a href="/api/login">
+      <Button variant="outline" size="sm">
+        <Text fontSize="sm">Sign In/Sign Up</Text>
+      </Button>
+    </a>
+  );
+  // } else {
+  //   return (
+  //     <NextLink href="/admin">
+  //       <Button pl="6px" pr="6px" variant="outline" size="sm">
+  //         <Avatar size="xs" mr="8px" name={data.name} src={data.picture} />
+  //         <Text fontSize="sm">{data.nickname}</Text>
+  //       </Button>
+  //     </NextLink>
+  //   );
 }
